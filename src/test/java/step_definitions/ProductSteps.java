1beae345c957242d5219d42fff9a47d71ff7643c
rafiimalfa <rafiimalfa@gmail.com>
2024-06-05 20:45:04 +0700

    Add Page Testing

---
+++ src/test/java/step_definitions/ProductSteps.java
@@ -0,0 +1,32 @@
+package step_definitions;
+
+import hooks.Hooks;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Then;
+import org.example.pageObject.ProductPage;
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+
+public class ProductSteps {
+    private WebDriver driver = Hooks.driver;
+    ProductPage productPage = new ProductPage(driver);
+
+    @Then("On the product page verify the saucelabs backpack is displayed")
+    public void onTheProductPageVerifyTheSaucelabsBackpackIsDisplayed() {
+        Assert.assertTrue(productPage.verifyTitleProductSauceLabsBackpack());
+    }
+    @And("Add to cart button saucelabs backpack is displayed")
+    public void addToCartButtonSaucelabsBackpackIsDisplayed() {
+        Assert.assertTrue(productPage.verifyAddToCartSauceLabsBackpack());
+
+    }
+
+    @And("Add to cart saucelabs backpack")
+    public void addToCartSaucelabsBackpack() {
+    }
+
+    @And("Remove button on the saucelabs backpack is displayed")
+    public void removeButtonOnTheSaucelabsBackpackIsDisplayed() {
+        Assert.assertTrue(productPage.verifyRemoveButtonDisplayedOnTheProductSauceLabsBackpack());
+    }
+}
